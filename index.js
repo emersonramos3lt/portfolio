18622a3e5d82bcf83fc1e34bcdc8c1905aee2ae4
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-05-26 18:00:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const mainMenu = document.getElementById("mainMenu");
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const content = document.querySelectorAll("nav .mainMenu li a");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+content.forEach(item => {
+    item.addEventListener("click", function() {
+        close();
+    })
+});
+
+function show() {
+    mainMenu.style.display = "flex";
+    mainMenu.style.opacity = 1;
+    mainMenu.style.pointerEvents = "all";
+}
+
+function close() {
+    mainMenu.style.opacity = "0";
+    mainMenu.style.pointerEvents = "none";
+}
+
+const words = ['Olá seja bem vindo! Eu sou Emerson'];
+
+words.forEach(word => {
+
+    let textTimeline = gsap.timeline({
+        yoyo: false
+    })
+
+    textTimeline.to('#typewriter', {
+        text: word,
+        duration: 3
+    })
+});
+
+const myLink = document.getElementById("myLink");
+
+myLink.addEventListener("mouseover", function() {
+    myLink.style.backgroundColor = "#fff"
+    myLink.style.color = "#111"
+    document.getElementById('myLink').innerHTML = `Ir para o GitHub <i class="fa-solid fa-arrow-up-right-from-square"></i>`
+});
+
+myLink.addEventListener("mouseout", function() {
+    myLink.style.backgroundColor = "#111"
+    myLink.style.color = "#fff"
+    myLink.textContent = "Ver mais projetos"
+})
