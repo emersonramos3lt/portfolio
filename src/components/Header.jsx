7b05920f5b4e333d55bdbd3c93f40c4a38575ba0
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-03-10 14:24:06 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
             <a href="/resume.pdf" aria-label='Download my resume' download className='uppercase text-sm sm:text-lg font-extrabold transition-colors hover:text-zinc-700' title='Download Resume'>Resume</a>
         </nav>
 
-        <h1 className='text-center text-3xl sm:text-6xl lg:text-8xl font-extrabold lg:leading-[120px] text-zinc-900 mt-14' data-aos="zoom-in-up" data-aos-duration="800">FRONTEND DEVELOPER WITH A PASSION FOR WEB DESIGN, UI/UX, AND CREATING STUNNING WEBSITES.</h1>
+        <h1 className='text-center text-3xl sm:text-6xl lg:text-8xl font-extrabold lg:leading-[120px] text-zinc-900 mt-14 uppercase' data-aos="zoom-in-up" data-aos-duration="800">Full Stack Developer with a passion for web design, UI/UX, and building stunning, user-friendly websites.</h1>
     </header>
   )
 }
