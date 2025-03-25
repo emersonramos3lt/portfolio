7a537e321e697dca02159b6acc699d17534a62ac
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-03-25 10:34:05 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <nav className='p-2 pt-5 sm:p-8 sm:pt-auto flex justify-between items-center' data-aos="fade-down" data-aos-duration="700">
             <span className='sm:text-[1.4rem] font-extrabold cursor-pointer uppercase'>Emerson Ramos<span className='text-rose-500'>.</span></span>
 
-            <a href="/resume.pdf" aria-label='Download my resume' download className='uppercase text-sm sm:text-lg font-extrabold transition-colors hover:text-zinc-700' title='Download Resume'>Resume</a>
+            <a href="/curriculo.pdf" aria-label='Download my resume' download className='uppercase text-sm sm:text-lg font-extrabold transition-colors hover:text-zinc-700' title='Download Resume'>Resume</a>
         </nav>
 
         <h1 className='text-center text-3xl sm:text-6xl lg:text-8xl font-extrabold lg:leading-[120px] text-zinc-900 mt-14' data-aos="zoom-in-up" data-aos-duration="800">FULL STACK DEVELOPER WITH A PASSION FOR WEB DESIGN, UI/UX, AND CREATING STUNNING APPLICATIONS.</h1>
