6cfa50318df74fcfbd515acb7c934fbb33dad576
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-04-11 09:41:23 -0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,11 +11,11 @@ const Projects = () => {
 
   return (
     <div className='mt-28 sm:mt-50 text-zinc-900 max-w-[90%] m-auto'>
-        <h2 className='text-center text-2xl sm:text-4xl lg:text-5xl font-extrabold mb-8' data-aos="fade-up">RELEVANT PROJECTS</h2>
+        <h2 className='text-center text-2xl sm:text-4xl lg:text-5xl font-extrabold mb-8' data-aos="fade-up" data-aos-duration="700">RELEVANT PROJECTS</h2>
 
         <div className='grid lg:grid-cols-2 gap-4'>
 
-            <div className='' data-aos="fade-right" data-aos-duration="2000">
+            <div className='' data-aos="fade-right" data-aos-duration="1000">
                 <a href="https://awwward-website.vercel.app/" target='_blank'>
                     <img src="/project1.png" alt="Awwwards Inspired React Website" className='
                     rounded-3xl w-[100%] h-[300px] sm:h-[500px] object-cover transition-transform hover:scale-95'/>
@@ -24,7 +24,7 @@ const Projects = () => {
                 <p className='font-medium text-zinc-600'>Website with advanced animations and responsive design.</p>
             </div>
 
-            <div className='' data-aos="fade-right" data-aos-duration="2500">
+            <div className='' data-aos="fade-right" data-aos-duration="1500">
                 <a href="https://movie-finder-react-psi.vercel.app/" target='_blank'>
                     <img src="/project2.png" alt="Movie Search with api" className='
                     rounded-3xl w-[100%] h-[300px] sm:h-[500px] object-cover transition-transform hover:scale-95'/>
@@ -33,7 +33,7 @@ const Projects = () => {
                 <p className='font-medium text-zinc-600'>A responsive React app that lets users search movies.</p>
             </div>
 
-            <div className='' data-aos="fade-right" data-aos-duration="2000">
+            <div className='' data-aos="fade-right" data-aos-duration="1000">
                 <a href="https://fullstack-blog-rho.vercel.app/" target='_blank'>
                     <img src="/project3.png" alt="Blog app" className='
                     rounded-3xl w-[100%] object-cover h-[300px] sm:h-[500px] transition-transform hover:scale-95'/>
@@ -42,7 +42,7 @@ const Projects = () => {
                 <p className='font-medium text-zinc-600'>A fullstack blogging application built with Next.js with MongoDB</p>
             </div>
 
-            <div className='' data-aos="fade-right" data-aos-duration="2500">
+            <div className='' data-aos="fade-right" data-aos-duration="1500">
                 <a href="https://nextjs-ecommerce-fullstack-gamma.vercel.app/" target='_blank'>
                     <img src="/project4.png" alt="Full-Stack E-Commerce" className='
                     rounded-3xl w-[100%] h-[300px] sm:h-[500px] object-cover transition-transform hover:scale-95'/>
