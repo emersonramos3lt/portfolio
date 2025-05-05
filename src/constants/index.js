cb51f3bee246d9386f1839b97cd12e5286927aed
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-05-05 16:20:52 -0300

    New

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+import project1 from "../../public/project1.png";
+import project2 from "../../public/project2.png";
+import project3 from "../../public/project3.png";
+import project4 from "../../public/project4.png";
+
+export const projectsItems = [
+  {
+    link: 'https://awwward-website.vercel.app/',
+    image: project1,
+    title: "Awwwards Inspired Website",
+    desc: "Website with advanced animations and responsive design.",
+  },
+  {
+    link: 'https://movie-finder-react-psi.vercel.app/',
+    image: project2,
+    title: "Movie Search with api",
+    desc: "A responsive React app that lets users search movies.",
+  },
+  {
+    link: 'https://fullstack-blog-rho.vercel.app/',
+    image: project3,
+    title: "React Social Media App",
+    desc: "A social media app built with React and Supabase.",
+  },
+  {
+    link: 'https://react-social-app-lpl4.vercel.app/',
+    image: project4,
+    title: "Agency Landing Page",
+    desc: "Developed using HTML, CSS, and JavaScript, the website delivers a smooth and engaging experience.",
+  },
+];
